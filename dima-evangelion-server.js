74bd7a4271ac61a761aaf159ecf8dea2dc5be7a6
Dima Duc <dimaduc@gmail.com>
2020-08-30 09:42:50 +0700

    da tao model eva unit

--- dima-evangelion-server.js
+++ dima-evangelion-server.js
@@ -1,3 +1,19 @@
 // đây là Express - Web Server cho website dima-evangelion
+const express = require('express');		    //phải mượn Express
+const evaRoutes = express.Router();	    //tạo Router để nhận tất cả câu hỏis
+
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');     //phải mượn Mongoose
+                                          //tìm và nói chuyện với MongoDB ở địa chỉ của nó
+const PORT = 5000;				                //địa chỉ Phòng
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/eva', evaRoutes);		        //bảo Router chỉ nhận câu hỏi bắt đầu ‘/hanhDong
+
+
+let evaUnitModel = require('./evaUnit.model');
 
 
---
+++ evaUnit.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+//tạo 1 Schema Model (giả)
+let evaUnitModel = new Schema(
+  {
+    name:{type: String},
+    pilot:{type: String}
+  }, 
+  { collection: 'eva_units'}           //tên của collection trong MongoDB
+
+
+
+);
+module.exports = mongoose.model('eva_units', evaUnitModel);
